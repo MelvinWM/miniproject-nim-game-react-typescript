47b431cbb4af28c5e556b6f2dd8c0b21e5fa4e57
MelvinWM <mithunder@hotmail.com>
2020-07-07 17:22:32 +0200

    Updates

---
+++ precache-manifest.de5a4585b9aeb2f43ce418b51f2405bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d19613cbd39f4834c4123b1a76237c",
+    "url": "/miniproject-nim-game-react-typescript/index.html"
+  },
+  {
+    "revision": "06677004af5c78f93f48",
+    "url": "/miniproject-nim-game-react-typescript/static/css/main.7a2c1303.chunk.css"
+  },
+  {
+    "revision": "c93f446bf68213276e1f",
+    "url": "/miniproject-nim-game-react-typescript/static/js/2.9534bb19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miniproject-nim-game-react-typescript/static/js/2.9534bb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06677004af5c78f93f48",
+    "url": "/miniproject-nim-game-react-typescript/static/js/main.87e84051.chunk.js"
+  },
+  {
+    "revision": "3e1a9a6851d503a6c405",
+    "url": "/miniproject-nim-game-react-typescript/static/js/runtime-main.48b2743c.js"
+  },
+  {
+    "revision": "2490c66d5a8255a7490ceee82b7cb0aa",
+    "url": "/miniproject-nim-game-react-typescript/static/media/matchstick.2490c66d.svg"
+  }
+]);
